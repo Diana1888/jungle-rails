573e7fdc6574db44c4904112a93623bf54c51a8a
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-12-22 23:58:32 -0500

    Add Feature Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_AUTH_USERNAME"], password: ENV["HTTP_AUTH_PASSWORD"]
+
   def show
+    @products = Product.count
+    @categories = Category.count
+
   end
 end
