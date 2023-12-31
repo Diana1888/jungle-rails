eb9b5496730ed3f69d34473db71dc5f5d1338865
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-12-30 21:09:52 -0500

    Add feauture User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,16 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
-
+  # About routes
   resources :about, only: [:index], controller: 'about'
+
+  # User routes
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
